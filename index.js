da8dc7110fc48bd4df1491a8035ba45ffc8f8350
yeti-detective <yeti.detective@gmail.com>
2017-05-20 12:35:15 -0400

    committing packages for deployment

--- index.js
+++ index.js
@@ -1,58 +1,62 @@
 "use strict"
 
+const express = require('express')
+const app = express()
+
 const osmosis = require('osmosis')
-const fs = require('fs')
+// const fs = require('fs')
 
-// let saveData = []
-let WL = {}
-let titles = []
-let links = []
-let imgs = []
-// let prices = []
+var port = process.env.PORT || 8000
 
-osmosis.get("https://www.amazon.com/gp/registry/wishlist/1PG248H308KJF/")
-  .set({
-    "Title": '#profile-list-name',
-    "WL_Items": ['#item-page-wrapper a@title'],
-    "WL_Links": ['#item-page-wrapper a@href'],
-    "WL_Imgs": ['#item-page-wrapper img@src'],
-    // "WL_prices": ['.a-color-price']
-  })
-  .log(console.log)
-  .data((data)=>{
-    // saveData.push(data)
-    WL.title = data.Title
-    data.WL_Items.map((name)=>{
-      if(!titles.includes(name)){
-        titles.push(name)
+app.get('/:wishlist', (req, res)=>{
+
+  let WL = {}
+  let titles = []
+  let links = []
+  let imgs = []
+  // let prices = []
+
+
+  osmosis.get("https://www.amazon.com/gp/registry/wishlist/" + req.params.wishlist)
+    .set({
+      "Title": '#profile-list-name',
+      "WL_Items": ['#item-page-wrapper a@title'],
+      "WL_Links": ['#item-page-wrapper a@href'],
+      "WL_Imgs": ['#item-page-wrapper img@src'],
+      // "WL_prices": ['.a-color-price']
+    })
+    .log(console.log)
+    .data((data)=>{
+      // saveData.push(data)
+      WL.title = data.Title
+      data.WL_Items.map((name)=>{
+        if(!titles.includes(name)){
+          titles.push(name)
+        }
+      })
+      data.WL_Links.map((link)=>{
+        if(link.indexOf('/dp/') == 0 && !links.includes('https://www.amazon.com' + link.substring(0, link.lastIndexOf('/')))){
+          links.push('https://www.amazon.com' + link.substring(0, link.lastIndexOf('/')))
+        }
+      })
+      data.WL_Imgs.map((pic)=>{
+        imgs.push(pic)
+      })
+    })
+    .done((data)=>{
+      for(var i = 0; i < titles.length; i++){
+        WL[i] = {
+          title: titles[i],
+          url: links[i],
+          img: imgs[i]
+        }
+
       }
+      res.json(JSON.stringify(WL))
     })
-    data.WL_Links.map((link)=>{
-      if(link.indexOf('/dp/') == 0 && !links.includes('https://www.amazon.com' + link.substring(0, link.lastIndexOf('/')))){
-        links.push('https://www.amazon.com' + link.substring(0, link.lastIndexOf('/')))
-      }
-    })
-    data.WL_Imgs.map((pic)=>{
-      imgs.push(pic)
-    })
-  })
-  .done((data)=>{
-    for(var i = 0; i < titles.length; i++){
-      WL[i] = {
-        title: titles[i],
-        url: links[i],
-        img: imgs[i]
-      }
 
-    }
-    console.log(JSON.stringify(WL))
-  
+})
 
-
-    /*
-    fs.writeFile('data.json', JSON.stringify( saveData, null, 4), (err)=>{
-      if(err) console.error(err)
-      else console.log('Data Saved to data.json file')
-    })
-    */
-  })
+app.listen(port, ()=>{
+  console.log('listening on ' + port)
+})
